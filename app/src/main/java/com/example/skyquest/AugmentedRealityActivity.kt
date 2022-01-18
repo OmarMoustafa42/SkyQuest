a96e9f5e23c884d41af3f1ef7410364ce4ee7d7d
OmarMoustafa42 <omarmoustafa42@gmail.com>
2022-01-19 04:08:37 +0800

    First Commit

---
+++ app/src/main/java/com/example/skyquest/AugmentedRealityActivity.kt
@@ -0,0 +1,11 @@
+package com.example.skyquest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AugmentedRealityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_augmented_reality)
+    }
+}
